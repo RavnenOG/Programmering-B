837674374d3cc796da287dad44a98731108bf29e
RavnenOG <emil.arp.knudsen@gmail.com>
2023-11-20 17:01:18 +0100

    cvxb

---
+++ 01-JuleOpgaveEmilMolin - Kopi/libraries/sketch.js
@@ -0,0 +1,34 @@
+
+function preload(){
+  Font1 = loadFont('Present.ttf')
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight,WEBGL);//Creates a canvas, with WEBGL so it can do 3D things
+  background(200) //Canvas background
+  angleMode(DEGREES)  //makes angels in Degrees
+}
+
+function draw() {
+  
+  //making of the appearance of the Christmas Calendar -
+  
+  //making the background for the Julekalender
+  fill(200,0,0)
+
+  rect(-width/2+20,-height/2+20,width-40,height-40)
+  
+  fill(0)
+  textSize(20)
+  textAlign(CENTER)
+  textFont(Font1)
+  text("Christmas Calendar",windowWidth/2-windowWidth/2,-windowHeight/2+60)
+  
+
+  // -
+  
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
+}
