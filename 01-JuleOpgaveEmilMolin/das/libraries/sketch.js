4ea8583fc9fa67126adff934fc53c43457d13ac3
RavnenOG <emil.arp.knudsen@gmail.com>
2023-12-10 14:06:45 +0100

    th

--- 01-JuleOpgaveEmilMolin/das/libraries/sketch.js
+++ 01-JuleOpgaveEmilMolin/das/libraries/sketch.js
@@ -3,25 +3,25 @@ function preload(){
   Font1 = loadFont('Present.ttf')
 }
 
- //Variables
- let doorsStopAngle = 130
- //Door 1's var
- let buttonOpener1
- let buttonAnimation1 = 0
- let openDoor1 = false
+ 
 
 function setup() {
   createCanvas(windowWidth,windowHeight,WEBGL);//Creates a canvas, with WEBGL so it can do 3D things
   background(200) //Canvas background
   angleMode(DEGREES)  //makes angels in Degrees
 
-  let col = color(0,0,0,0)//Transparent color
-  //Door 1
-  
+ 
 }
 
 function draw() {
+ 
+  let col = color(0,0,200,0)//Transparent color
+  let sizeX = windowWidth/20;
+  let sizeY = windowHeight/12;
+  let posX = windowWidth/6;
+  let posY = windowHeight/4;
   
+
     //making of the appearance of the Christmas Calendar -
   
   //background for the Julekalender
@@ -37,11 +37,13 @@ function draw() {
   
     //The 24 gates
 
-  //Door 1
-
-  
   
 }
+
+function mousePressed(){
+  
+}
+
 
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
