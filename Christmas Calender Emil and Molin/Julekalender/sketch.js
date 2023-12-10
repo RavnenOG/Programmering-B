4f6c9e0c75ad79eba2ec67244ccfe181ae246f23
RavnenOG <emil.arp.knudsen@gmail.com>
2023-12-10 16:37:24 +0100

    hehehe haaa

--- Christmas Calender Emil and Molin/Julekalender/sketch.js
+++ Christmas Calender Emil and Molin/Julekalender/sketch.js
@@ -2,19 +2,37 @@
 let size = 120;
 let gateOpenArray = new Array();
 let gateOpenPermArray = new Array();
-let angleArray;
+let angleArray = new Array();
 let rotationBegunArray = new Array();
 
 let gateXArray = new Array();
 let gateYArray = new Array();
+
+let calendarOpen = false;
 
 //gateMaker laver alle lågerne
 function gateMaker(x,y,gateImage,gateNumber){
 
   // vi gemmer translate og rotateY default state så vi kan ændre det tilbage til det
   push()
+
   //Bruger array for at sætte variablen til den låge vi er noget til
   let rotationBegun = rotationBegunArray[gateNumber-1]
+
+  //Ser om man klikker på lågen
+  if (mouseX > x-size/2+windowWidth/2 && mouseX < x+size/2+windowWidth/2 && mouseY > y-size/2+windowHeight/2 && mouseY < y+size/2+windowHeight/2 && mouseIsPressed == true){
+    rotationBegunArray[number-1] = true;
+  }
+   
+
+  //Laver noget bag lågen
+  fill(0)
+  rect(x-size/2,y-size/2,size)
+
+  translate(x-size/2,y-size/2,0)
+
+  //Vi bruger så pop til at ændre translate og rotate tilbage til den originale
+ pop()
 }
 
 function setup(){
@@ -23,7 +41,14 @@ function setup(){
 }
 
 function draw(){
-background(180);
+  if(calendarOpen == true){
+    background(180);
+
+    //Laver lågerne
+    gateMaker(gateXArray[0],gateYArray[0],image1);
+
+  }
+
 
 }
 
