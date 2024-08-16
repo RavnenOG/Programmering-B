f8705dff0d597b68f72114a34bed07e59a98e64e
RavnenOG <emil.arp.knudsen@gmail.com>
2024-08-16 09:54:13 +0200

    sss

--- GameIdeaTest1/sketch.js
+++ GameIdeaTest1/sketch.js
@@ -1,3 +1,5 @@
+
+//Images
 let playerSkin
 
 
@@ -8,17 +10,23 @@ playerSkin = loadImage("Images/TestPlayerSkin.PNG")
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+
+  createCanvas(windowWidth-1,windowHeight-1);//-1 is written cause, otherwise somehow it makes scrolling bars
 
   player = {
     w:100,
     h:100,
     x:width/2,
-    y:height-this.h,
+    y:height-50,
+    s: 5,
     move: function(directionX){
-
+      this.x += directionX * this.s
+    },
+    jump: function(){
+      
     },
     show: function(){
+      imageMode(CENTER)
       image(playerSkin, this.x, this.y, this.w, this.h)
     }
   }
@@ -26,13 +34,12 @@ function setup() {
 
 function draw() {
   background(220);
-
   //Player functions
   if(keyIsDown(LEFT_ARROW)){
-    player.move(-1,0)
+    player.move(-1)
   }
   if(keyIsDown(RIGHT_ARROW)){
-    player.move(+1,0)
+    player.move(+1)
   }
   
   player.show()
